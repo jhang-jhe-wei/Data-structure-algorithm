2fcca1aadd4b6fabbf812b809facd450194d91cd
wells <jhang0912407249@gmail.com>
2024-07-11 15:32:23 +0900

    feat: implement binary_search_tree

--- spec/binary_search_tree_spec.rb
+++ spec/binary_search_tree_spec.rb
@@ -1,11 +1,125 @@
 # frozen_string_literal: true
+require 'byebug'
+
+class Node
+  attr_accessor :right, :left, :value, :parent
+
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @right = nil
+    @left = nil
+  end
+
+  def to_s
+    value
+  end
+end
 
 class BinarySearchTree
-  def initialize; end
-  def insert(_); end
-  def find(_); end
-  def delete(_); end
-  def inorder; end
+  attr_accessor :root
+
+  def initialize
+    @root = nil
+  end
+
+  def insert(value)
+    new_node = Node.new(value)
+    if root.nil?
+      self.root = new_node
+      return
+    end
+
+    node = root
+    loop do
+      if new_node.value < node.value
+        if node.left.nil?
+          new_node.parent = node
+          node.left = new_node
+          return
+        else
+          node = node.left
+        end
+      else
+        if node.right.nil?
+          new_node.parent = node
+          node.right = new_node
+          return
+        else
+          node = node.right
+        end
+      end
+    end
+  end
+
+  def find(value)
+    !!find_node_by_value(value)
+  end
+
+  def delete(value)
+    node = find_node_by_value(value)
+    parent_node = node.parent
+
+    if node.right.nil? && node.left.nil? && node == root
+      self.root = nil
+      return
+    end
+
+    # node has no children
+    if node.right.nil? && node.left.nil?
+      parent_node.right == node ? parent_node.right = nil : parent_node.left = nil
+      return
+    end
+
+    # node has children
+    # find the biggest node and replace with the node
+    biggest_node = node.left
+    biggest_node = biggest_node.right until biggest_node.right.nil?
+
+    node.value = biggest_node.value
+    biggest_node.parent_node.right = nil
+  end
+
+  def inorder
+    ary = []
+    stack = []
+    node = root
+
+    until node.nil?
+      stack.push(node)
+      node = node.left
+    end
+
+    until stack.empty?
+      node = stack.pop
+      ary.push(node.value)
+      next if node.right.nil?
+
+      node = node.right
+      until node.nil?
+        stack.push(node)
+        node = node.left
+      end
+    end
+    ary
+  end
+
+  private
+
+  def find_node_by_value(value)
+    node = root
+    until node.nil?
+      case value <=> node.value
+      when -1 # smallar
+        node = node.left
+      when 0 # equal
+        return node
+      when 1 # bigger
+        node = node.right
+      end
+    end
+    nil
+  end
 end
 
 RSpec.describe BinarySearchTree do
