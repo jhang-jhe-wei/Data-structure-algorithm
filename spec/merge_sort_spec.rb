fa47492aa81a02a51650acde2304cc3694434d25
wells <jhang0912407249@gmail.com>
2024-07-11 17:36:39 +0900

    test: test edges for merge sort

--- spec/merge_sort_spec.rb
+++ spec/merge_sort_spec.rb
@@ -1,33 +1,32 @@
 # frozen_string_literal: true
 
-module MergeSort
-  class << self
-    def sort(ary)
-      return ary if ary.size <= 1
-
-      left, right = slice(ary).map { |a| sort(a) }
-      merge(left, right)
-    end
-
-    private
-
-    def slice(ary)
-      return [ary] if ary.size <= 1
-
-      half = ary.size / 2
-      [ary[0...half], ary[half..]]
-    end
-
-    def merge(left, right)
-      result = []
-      result << (left.first <= right.first ? left.shift : right.shift) until left.empty? || right.empty?
-      result + left + right
-    end
-  end
-end
-
 RSpec.describe MergeSort do
   it 'sorts an array' do
     expect(MergeSort.sort([3, 2, 1])).to eq([1, 2, 3])
+  end
+
+  it 'sorts an array with duplicate values' do
+    expect(MergeSort.sort([3, 2, 1, 2])).to eq([1, 2, 2, 3])
+  end
+
+  it 'sorts an array with negative values' do
+    expect(MergeSort.sort([3, -2, 1, -1])).to eq([-2, -1, 1, 3])
+  end
+
+  it 'sorts an array with one value' do
+    expect(MergeSort.sort([1])).to eq([1])
+  end
+
+  it 'sorts an array with two values' do
+    expect(MergeSort.sort([2, 1])).to eq([1, 2])
+  end
+
+  it 'returns an empty array when given an empty array' do
+    expect(MergeSort.sort([])).to eq([])
+  end
+
+  it 'sorts a large array' do
+    large_array = Array.new(1000) { rand(1..1000) }
+    expect(MergeSort.sort(large_array)).to eq(large_array.sort)
   end
 end
